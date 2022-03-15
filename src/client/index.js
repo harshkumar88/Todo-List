fcdfe7ad59e5e051a7c72887b9b4a250b520dea5
harshkumar <harsh1559.be20@gmail.com>
2022-03-15 12:31:29 +0530

    ji

--- src/client/index.js
+++
@@ -1,5 +0,0 @@
-const publicVapidKey = 'BFGvRdujOYr5xrnNTPwyoGDapjUjrBVMZ_JCb3OPudbkewAbN7gq377HgvbXUW5iXzhN7rXbyiya6WJcjjc2Ni8';
-const privateVapidKey = 'KFnYE_ShbG8emME5EQfYZtXgmIBuYstxoN64wfaUzpo';
-
-//setting vapid keys details
-webpush.setVapidDetails('mailto:mercymeave@section.com', publicVapidKey,privateVapidKey);
